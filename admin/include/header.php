dedf8a711caac3559ea7d1a4655128e1208bca6b
YajiHub <jopurjay23@gmail.com>
2025-05-26 21:35:28 +0800

    logo

--- admin/include/header.php
+++ admin/include/header.php
@@ -21,7 +21,7 @@
     <!-- Navigation Bar -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo mr-5" href="dashboard.php"><img src="../images/logo.svg" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo mr-5" href="dashboard.php"><img src="../images/updocs.png" class="mr-2" alt="logo"/></a>
         <a class="navbar-brand brand-logo-mini" href="dashboard.php"><img src="../images/logo-mini.svg" alt="logo"/></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
